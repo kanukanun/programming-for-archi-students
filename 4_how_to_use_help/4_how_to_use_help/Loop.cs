a9a6e9e622f4c1c355edbbb826eb0a1852479a07
yasushisakai <ysshski@gmail.com>
2015-02-13 15:01:02 +0900

    added how to use SDK

---
+++ 4_how_to_use_help/4_how_to_use_help/Loop.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rhino;
+using Rhino.Geometry;
+
+namespace _4_how_to_use_help
+{
+    class Loop : Rhino_Processing
+    {
+
+        // declare variables
+        
+        // the origin, in other words the position of the sun
+        Point3d origin;
+
+        // two orbits;
+        Sphere earth;
+        Sphere moon;
+
+        double earth_r,moon_r; // earth and moon radius 
+        double sun_earth_distance,earth_moon_distance;
+
+        public override void Setup()
+        {
+
+            origin = new Point3d(0.0,0.0,0.0);
+
+            earth_r = 63.71; // actually its 6,371km
+            moon_r = 17.37; 
+
+       
+            sun_earth_distance = 1496.00; // actually its 149,600,000km
+            earth_moon_distance = 384.00; // actually its     384,000km
+
+            // make new sphere
+            earth = new Sphere(new Point3d(0.0,sun_earth_distance,0.0),earth_r);
+            moon = new Sphere(new Point3d(0.0,sun_earth_distance+earth_moon_distance,0.0),moon_r);
+
+        }
+
+        public override void Draw()
+        {
+
+            // you need the up vector to determine the rotation axis 
+            //Vector3d up_vector = new Vector3d(0.0,0.0,1.0);
+            // this is the same as Vector3d.ZAxis, which always returns new Vector(0.0,0.0,1.0)
+            
+            // rotate the earth sphere (radians,rotation_axis,rotation_origin)
+            double base_speed = 0.1;
+            earth.Rotate(RhinoMath.ToRadians(base_speed), Vector3d.ZAxis, origin);
+
+            // add earth to document
+            doc.Objects.AddSphere(earth);
+
+            //moon speed ratio (orbit time = earth : 365.4 days, moon : 27.32days) 
+            double moon_speed = 365.4 / 27.32;
+
+            // you can access to the spheres center by getting the .Center property.
+            // rotation the moon sphere
+            
+            moon.Rotate(RhinoMath.ToRadians(base_speed * moon_speed), Vector3d.ZAxis, earth.Center);
+
+            // add moon to document
+            doc.Objects.AddSphere(moon);
+
+        }
+
+    }
+}
---
+++ 4_how_to_use_help/4_how_to_use_help/howtousehelpCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace _4_how_to_use_help
+{
+    [System.Runtime.InteropServices.Guid("97a31f01-c518-4427-8944-cd6032a0cb80")]
+    public class howtousehelpCommand : Command
+    {
+        public howtousehelpCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static howtousehelpCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "howtousehelpCommand"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            Loop loop = new Loop();
+
+            loop.Run(doc);
+
+            return Result.Success;
+        }
+    }
+}
