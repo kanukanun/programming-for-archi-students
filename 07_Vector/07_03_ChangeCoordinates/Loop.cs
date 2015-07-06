1ea610b9497ef17d91d0d3cc2080b45fab19557f
KazunoriNakayama <kanukanun@gmail.com>
2015-07-06 11:45:24 +0900

    small correction

--- 07_Vector/07_03_ChangeCoordinates/Loop.cs
+++ 07_Vector/07_03_ChangeCoordinates/Loop.cs
@@ -17,7 +17,7 @@ namespace _5.Classes
         {
             RhinoView view;
             RhinoGet.GetView("select viewport", out view);//ビューポートを選択し設定を格納
-            objs = new BoxProject(100 , view);//今回対象とするオブジェクト
+            objs = new ChangeCoordinates(100 , view);//今回対象とするオブジェクト
 
             objs.MakeBox();
             objs.Area();
