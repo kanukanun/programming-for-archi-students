f2272d6914e6ff722647e4b56320f4ad45989812
kanukanun <kanukanun@gmail.com>
2015-07-21 18:02:05 +0900

    addes

---
+++ 05_Classes/05_02_01/AstroObject.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rhino;
+using Rhino.Geometry;
+
+namespace _05_02_01_AstroObject
+{
+    class AstroObject : Rhino_Processing
+    {
+        //////////////
+        //properties//
+        //////////////
+        private int id;
+        private string name;
+        private double radius;
+        private Point3d rotation_origin;
+        private double rotation_radius;
+        private double rotation_days;
+
+        private Point3d position;
+        private Sphere obj;
+
+        /////////////////
+        //constructors//
+        ////////////////
+        public AstroObject(
+            int _id,
+            string _name,
+            double _radius,
+            Point3d _rotation_origin,
+            double _rotation_radius,
+            double _rotation_days
+            )
+        {
+
+            // set variables
+            id = _id;
+            name = _name;
+            radius = _radius;
+            rotation_origin = _rotation_origin;
+            rotation_radius = _rotation_radius;
+            rotation_days = _rotation_days;
+        }
+
+
+        ///////////
+        //methods//
+        ///////////
+        public void Rotate()
+        {
+            double X = rotation_origin.X + Math.Cos(frame_no / rotation_days * Math.PI + 90) * rotation_radius;
+            double Y = rotation_origin.Y + Math.Sin(frame_no / rotation_days * Math.PI + 90) * rotation_radius;
+
+            position = new Point3d(X, Y, 0);   
+        }
+
+        public void Display(RhinoDoc _doc)
+        {
+            obj = new Sphere(position, radius);
+            _doc.Objects.AddSphere(obj);
+        }
+
+        public Point3d Center()
+        {
+            return position;
+        }
+
+        public void UpdateCenter(Point3d _center)
+        {
+            rotation_origin = _center;
+        }
+
+    }
+}
---
+++ 05_Classes/05_04_/Loop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rhino;
+using Rhino.Geometry;
+
+namespace _05_02_01_AstroObject
+{
+    class Loop : Rhino_Processing
+    {
+        AstroObject sun, venus, earth, moon, mars, jupiter, saturn; // declare our home planet and it's oribit.
+
+        public override void Setup()
+        {
+            venus = new AstroObject(0, "venus", 60.51, new Point3d(), 1082.0, 224.4);
+            earth = new AstroObject(1, "earth", 63.71, new Point3d(), 1496.0, 365.4);
+            moon = new AstroObject(2, "moon", 17.37, earth.Center(), 384.0, 27.32);
+            mars = new AstroObject(3, "mars", 33.97, new Point3d(), 2279.3, 686.5);
+            jupiter = new AstroObject(4, "jupiter", 699.11, new Point3d(), 7785.0, 4329.5);
+            saturn = new AstroObject(5, "saturn", 602.68, new Point3d(), 14267.2, 10752.9);
+        }
+
+        public override void Draw()
+        {
+            earth.Rotate();
+            earth.Display(doc);
+            venus.Rotate();
+            venus.Display(doc);
+            mars.Rotate();
+            mars.Display(doc);
+            jupiter.Rotate();
+            jupiter.Display(doc);
+            saturn.Rotate();
+            saturn.Display(doc);
+            // we need to update the center of the moon
+            moon.UpdateCenter(earth.Center());
+
+            moon.Rotate();
+            moon.Display(doc);
+        }
+
+    }
+}
