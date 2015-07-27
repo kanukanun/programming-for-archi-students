40be6da8e52eff576d8651ae42470604d2e6c8bb
KazunoriNakayama <kanukanun@gmail.com>
2015-07-27 16:39:36 +0900

    correction

--- 03_HelloWorld/03_01-ans04/Loop.cs
+++ 03_HelloWorld/03_01-ans04/Loop.cs
@@ -10,32 +10,30 @@ namespace _03_01_template
 {
     class Loop : Rhino_Processing
     {
-        Point3d earth;
-        Point3d moon;   //課題1
-        double distance = 10.0; //課題3
-        double DistanceBetweenMoonAndEarth = 10.0 + 2;      //課題4 型 変数名（地球と月の距離） = 数値（固定）
-        double A = 13.4;    //課題4　（地球の公転周期）÷（月の交点周期）
+        Point3d earth;  // 型 変数名;
+        Point3d moon;   // 型 変数名;
+        double distance = 10.0; //型 変数名（地球の公転半径） = 数値（固定）
+        double DistanceBetweenMoonAndEarth = 10.0 + 2;      //型 変数名（地球と月の距離） = 数値（固定））
+        double A = 13.4;    //　（地球の公転周期）÷（月の交点周期）
 
-        public override void Setup()
+        public override void Setup()    //Setup関数
         {
-            earth = new Point3d(0.0, distance, 0.0);//課題3
-            moon = new Point3d(0.0, distance + DistanceBetweenMoonAndEarth, 0.0);     //課題1
+            earth = new Point3d(0.0, distance, 0.0);    //最初の地球の位置を指定
+            moon = new Point3d(0.0, distance + DistanceBetweenMoonAndEarth, 0.0);     //最初の月の位置を指定
         }
 
-        public override void Draw()
+        public override void Draw()     //Draw関数
         {
-            earth.X = Math.Cos(frame_no / 180.0 * Math.PI) * distance;
-            earth.Y = Math.Sin(frame_no / 180.0 * Math.PI) * distance;
+            earth.X = Math.Cos(frame_no / 180.0 * Math.PI) * distance;      // frame_noに応じたearthのX座標を指定
+            earth.Y = Math.Sin(frame_no / 180.0 * Math.PI) * distance;      // frame_noに応じたearthのY座標を指定
 
-            //moon.X = Math.Cos(frame_no / 180.0 * Math.PI) * distance;   //課題1
-            //moon.Y = Math.Sin(frame_no / 180.0 * Math.PI) * distance;   //課題1
-            moon.X = earth.X + Math.Cos(frame_no / 180.0 * Math.PI * A);    //課題4 frame_noに応じたmoonのX座標を指定
-            moon.Y = earth.Y + Math.Sin(frame_no / 180.0 * Math.PI * A);    //課題4 frame_noに応じたmoonのY座標を指定
+            moon.X = earth.X + Math.Cos(frame_no / 180.0 * Math.PI * A);    // frame_noに応じたmoonのX座標を指定
+            moon.Y = earth.Y + Math.Sin(frame_no / 180.0 * Math.PI * A);    // frame_noに応じたmoonのY座標を指定
 
             doc.Objects.AddPoint(earth);
             doc.Objects.AddPoint(moon);
 
-            RhinoApp.WriteLine(String.Format("{0}", frame_no));     //課題2
+            RhinoApp.WriteLine(String.Format("{0}", frame_no));
         }
     }
 }
