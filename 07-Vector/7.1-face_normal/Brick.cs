7a34fa99eae810d45b949590a9e7be598435ffa0
KazunoriNakayama <kanukanun@gmail.com>
2015-08-04 11:52:29 +0900

    added comment

--- 07-Vector/7.1-face_normal/Brick.cs
+++ 07-Vector/7.1-face_normal/Brick.cs
@@ -11,6 +11,10 @@ namespace _5.Classes
 {
     class Brick
     {
+        //////////////
+        //properties//
+        //////////////
+        
         private int id;
         private double height;
         private double angle;
@@ -18,18 +22,26 @@ namespace _5.Classes
         private Point3d a , b , c , d , a1, b1, c1, d1;
         private NurbsSurface srf, srf1, srf2, srf3, srf4;
 
+        ////////////////
+        //constructors//
+        ////////////////
 
-         public Brick(
-            int _id,
-             double _height,
-            double _angle
+        public Brick(
+        int _id,
+        double _height,
+        double _angle
         )
         {
             id = _id;
             height = _height; 
             angle = _angle;
         }
+        
+        ///////////
+        //methods//
+        ///////////
 
+        //一番目のSurfaceを作る
         public void MakeStartSrf()
         {
             a = new Point3d(0, 0, 0);
@@ -40,20 +52,24 @@ namespace _5.Classes
             srf = NurbsSurface.CreateFromCorners(a, b, c, d);
         }
 
+        //上面を作る
         public void MakeSrf(List<Brick> brick)
         {
             for (int i = 0; i < id; i++)
             {
+                //底面の法線ベクトル方向へ移動
                 srf.Translate(brick[i].NormalVector() * height);
             }
         }
 
+        //任意の3つの点でできる三角形の面積を求める
         private double TriangleArea(Point3d p0 , Point3d p1 , Point3d p2)
         {
             double s;
             double half;
             Line l1 , l2 , l3;
 
+            //ヘロンの公式を使って面積を求める
             l1 = new Line(p0 , p1);
             l2 = new Line(p1 , p2);
             l3 = new Line(p2 , p0);
@@ -62,14 +78,19 @@ namespace _5.Classes
             
             return s;
         }
-        private Vector3d NormalVector()//get normal vector in surface
+        
+        //Surfaceの法線ベクトルを求める
+        private Vector3d NormalVector()
         {
             Vector3d normvec , ab , ad , cd , cb;
+         
+            //Surfaceの各頂点に点を生成
             a1 = srf.PointAt(0 , 0);
             b1 = srf.PointAt(100 , 0);
             c1 = srf.PointAt(100 , 200);
             d1 = srf.PointAt(0 , 200);
 
+            //Surfaceを重なり合わない2つの三角形に分割し、それぞれ面積を求める
             double s1 = TriangleArea(a1 , b1 , d1);
             double s2 = TriangleArea(c1 , d1 , b1);
 
@@ -80,18 +101,21 @@ namespace _5.Classes
             cd = new Vector3d(d1) - new Vector3d(c1);
             cb = new Vector3d(b1) - new Vector3d(c1);
 
-            normvec = Vector3d.CrossProduct(ab , ad) * (s1 / sum);
+            //任意の頂点に対する法線ベクトルを求め面積比をかけ足し合わせる
+            normvec = Vector3d.CrossProduct(ab, ad) * (s1 / sum);
             normvec += Vector3d.CrossProduct(cd, cb) * (s2 / sum);
             
             return normvec / normvec.Length;
         }
 
+        //底面の中心座標を求める
         private Point3d Center()
         {
             Point3d center = srf.PointAt(50 , 100);
             return center;
         }
 
+        //Surfaceを回転させ積み上げていく
         public void MoveSrf(int num)//pile of brick
         {
             double ang = RhinoMath.ToRadians((angle / (num - 1)) * id);
