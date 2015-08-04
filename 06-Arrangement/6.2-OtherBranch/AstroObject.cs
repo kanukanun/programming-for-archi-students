87b149551a0e78e45bbdc0410f87100e2018c14c
KazunoriNakayama <kanukanun@gmail.com>
2015-08-04 13:16:36 +0900

    correction

--- 06-Arrangement/6.2-OtherBranch/AstroObject.cs
+++ 06-Arrangement/6.2-OtherBranch/AstroObject.cs
@@ -10,7 +10,10 @@ namespace _5.Classes
 {
     class AstroObject : Rhino_Processing
     {
-        //properties
+        //////////////
+        //properties//
+        //////////////
+        
         public double radius;
         public Point3d rotation_origin;
         public double rotation_radius;
@@ -19,7 +22,10 @@ namespace _5.Classes
         private Point3d position;
         private Sphere obj;
 
-        // constructors
+        ////////////////
+        //constructors//
+        ////////////////
+        
         public AstroObject(
             double _radius,
             Point3d _rotation_origin,
@@ -33,39 +39,19 @@ namespace _5.Classes
             rotation_origin = _rotation_origin;
             rotation_radius = _rotation_radius;
             rotation_days = _rotation_days;
-
-            // 
-            
-
         }
 
-
-        //methods
+        ///////////
+        //methods//
+        ///////////
+        
         public void Rotate()
         {
             double X = rotation_origin.X + Math.Cos(frame_no / rotation_days * Math.PI + 90) * rotation_radius;
             double Y = rotation_origin.Y + Math.Sin(frame_no / rotation_days * Math.PI + 90) * rotation_radius;
 
             position = new Point3d(X, Y, 0);   
-
-            //RhinoApp.WriteLine(String.Format("{0}", frame_no));
-            //RhinoApp.WriteLine(position1.ToString());
         }
-
-        //public void Rotate2(RhinoDoc _doc)
-        //{
-        //    double X = Math.Cos(frame_no / 180.0 * Math.PI + 90) * rotation_radius;
-        //    double Y = Math.Sin(frame_no / 180.0 * Math.PI + 90) * rotation_radius;
-
-        //    double X1 = X + Math.Cos(frame_no / 180.0 * Math.PI + 90);
-        //    double Y1 = Y + Math.Sin(frame_no / 180.0 * Math.PI + 90);
-
-        //    Point3d position2 = new Point3d(X1, Y1, 0);
-
-        //    RhinoApp.WriteLine(position2.ToString());
-
-        //    _doc.Objects.AddPoint(position2);
-        //}
 
         public void Display(RhinoDoc _doc)
         {
